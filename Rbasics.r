7bf8db05aad1d24d808402bf536216fe0dc45acb
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2020-09-05 21:16:34 -0300

    Update and rename ArithmeticWithR.r to Rbasics.r

---
+++ Rbasics.r
@@ -0,0 +1,37 @@
+# An addition
+5 + 5 
+
+# A subtraction
+5 - 5 
+
+# A multiplication
+3 * 5
+
+ # A division
+(5 + 5) / 2 
+
+# Exponentiation
+2^5
+
+# Modulo
+28 %% 6
+
+# Assign a value to the variable my_apples
+my_apples <- 5 
+
+# Fix the assignment of my_oranges
+my_oranges <- 6 
+
+# Create the variable my_fruit and print it out
+my_fruit <- my_apples + my_oranges 
+my_fruit
+
+# Change my_numeric to be 42
+my_numeric <- 42
+
+# Change my_character to be "universe"
+my_character <- "universe"
+
+# Change my_logical to be FALSE
+my_logical <- FALSE
+
